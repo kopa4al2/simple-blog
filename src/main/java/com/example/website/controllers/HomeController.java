59d7211284d44b2330b0ec81189d48f8031a168d
stefan <a582hs@gmail.com>
2017-12-13 22:55:51 +0200

    3

--- src/main/java/com/example/website/controllers/HomeController.java
+++ src/main/java/com/example/website/controllers/HomeController.java
@@ -1,4 +1,22 @@
 package com.example.website.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+//    @RequestMapping("/")
+//    @ResponseBody
+//    public String showHome() {
+//        return "<div> Home </div>   <button>Buton</button";
+//    }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String showLogin(){
+        return "<form><input type=text/> <button>login</button>";
+    }
 }
