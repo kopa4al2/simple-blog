9ddacb2472f8154de8cdec3bfbe9660af28b1b67
stefan <a582hs@gmail.com>
2017-12-14 10:54:38 +0200

    template

--- src/main/java/com/example/website/controllers/HomeController.java
+++ src/main/java/com/example/website/controllers/HomeController.java
@@ -1,18 +1,22 @@
 package com.example.website.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @Controller
 public class HomeController {
 
-//    @RequestMapping("/")
-//    @ResponseBody
-//    public String showHome() {
-//        return "<div> Home </div>   <button>Buton</button";
-//    }
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    String home(Model model) {
+        return "index";
+    }
 
     @RequestMapping("/login")
     @ResponseBody
