c2550c7929011c45f5d99d0652036e12a9bf15be
stefan <a582hs@gmail.com>
2017-12-14 18:03:45 +0200

    db connection

--- src/main/java/com/example/website/DemoApplication.java
+++ src/main/java/com/example/website/DemoApplication.java
@@ -8,10 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException, SQLException {
+//		getConnection();
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
 	}
 }
